99e0540f2708ef446dcb9f70a63562fc719650d3
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-11-06 14:42:30 -0800

    fixed type

--- config/initializers.rb
+++ config/initializers.rb
@@ -16,7 +16,7 @@ Bridgetown.configure do |config|
   #
   # init :dotenv
   #
-  init :"brigetown-prismic"
+  init :"bridgetown-prismic"
 
   # Uncomment to use Bridgetown SSR (aka dynamic rendering of content via Roda):
   #
