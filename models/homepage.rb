d43b9de9b59e83c324f434ca302b50817fdb8233
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-11-06 22:22:35 -0800

    updated provided data for front matter and prismic mappings

--- models/homepage.rb
+++ models/homepage.rb
@@ -1,6 +1,6 @@
 class Homepage < Bridgetown::Model::Base
   class << self
-    # def collection_name = :pages
+    def collection_name = :pages
     def prismic_custom_type = :homepage
     def prismic_slug(doc) = doc.slug
     def prismic_url(doc)
@@ -16,6 +16,8 @@ class Homepage < Bridgetown::Model::Base
       created_at      doc.first_publication_date
 
       layout          :home
+      permalink       doc["homepage.permalink"]   .as_text
+      page_class      doc["homepage.page_class"]  .as_text
       title           doc["homepage.title"]       .as_text
       content         doc["homepage.body"]        &.as_html with_links
     end
