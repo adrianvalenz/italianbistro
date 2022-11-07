f0ab90c4b0f8b6271c846caed2fa742be226d208
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-11-06 19:35:11 -0800

    removed homepage model and added a page model

--- models/page.rb
+++ models/page.rb
@@ -1,10 +1,10 @@
-class Homepage < Bridgetown::Model::Base
+class Page < Bridgetown::Model::Base
   class << self
     def collection_name = :pages
-    def prismic_custom_type = :homepage
+    def prismic_custom_type = :pages
     def prismic_slug(doc) = doc.slug
     def prismic_url(doc)
-      "/"
+      "/#{prismic_slug(doc)}"
     end
   end
 
@@ -15,9 +15,9 @@ class Homepage < Bridgetown::Model::Base
       type              doc.type
       created_at        doc.first_publication_date
 
-      layout            :home
-      title             doc["homepage.title"]         .as_text
-      content           doc["homepage.homepage_body"] &.as_html with_links
+      layout            :page
+      title             doc["page.title"]           .as_text
+      content           doc["page.page_body"]       &.as_html with_links
     end
   end
 end
