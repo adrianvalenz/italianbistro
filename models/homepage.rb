b8a77bc285588055200f1d4682f41974bae96551
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-11-06 22:07:19 -0800

    added homepage model

---
+++ models/homepage.rb
@@ -0,0 +1,23 @@
+class Homepage < Bridgetown::Model::Base
+  class << self
+    # def collection_name = :pages
+    def prismic_custom_type = :homepage
+    def prismic_slug(doc) = doc.slug
+    def prismic_url(doc)
+      "/"
+    end
+  end
+
+  def self.process_prismic_document(doc)
+    provide_data do
+      id              doc.id
+      slug from: ->   { prismic_slug(doc) }
+      type            doc.type
+      created_at      doc.first_publication_date
+
+      layout          :home
+      title           doc["homepage.title"]       .as_text
+      content         doc["homepage.body"]        &.as_html with_links
+    end
+  end
+end
